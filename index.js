dc72fa4c70294cc170662f447eac474bcf805eab
Denis Korir <denohrkoryr@gmail.com>
2024-07-19 20:13:17 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const mainElement = document.querySelector("main");
+  books.forEach((book) => {
+    const bookElement = document.createElement("div");
+    bookElement.textContent = book.name;
+    mainElement.appendChild(bookElement);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener("DOMContentLoaded", fetchBooks);
